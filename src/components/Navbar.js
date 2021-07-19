19035c1729292d283c0b1df6b7ca3c858d9e2348
Trace <tracedelange@me.com>
2021-07-19 10:20:02 -0500

    Inital Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div id='navigation'>
+
+            <button>Home</button>
+            <button>Projects</button>
+            <button>Contact</button>
+            
+        </div>
+    )
+}
+
+export default Navbar;
