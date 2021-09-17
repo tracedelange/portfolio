89e933f417565fbea154ce12bd493d77ce92b5e3
Trace <tracedelange@me.com>
2021-09-17 15:35:06 -0500

    styling updates and resume tweaks

--- src/components/ExTab.js
+++ src/components/ExTab.js
@@ -107,21 +107,22 @@ export default function VerticalTabs() {
         aria-label="Vertical tabs example"
         className={classes.tabs}
       >
-        <Tab className={classes.tabs}
-          label="College Education"
-          {...a11yProps(0)} />
-
-        <Tab className={classes.tabs}
-          label="Post Graduate Work"
-          {...a11yProps(1)} />
-
-        <Tab className={classes.tabs}
-          label="Software education"
-          {...a11yProps(2)} />
 
         <Tab className={classes.tabs}
           label="freelance development"
           {...a11yProps(3)} />
+        <Tab className={classes.tabs}
+          label="Software education"
+          {...a11yProps(2)} />
+        <Tab className={classes.tabs}
+          label="Post Graduate Work"
+          {...a11yProps(1)} />
+        <Tab className={classes.tabs}
+          label="College Education"
+          {...a11yProps(0)} />
+
+
+
 
         <Link className={classes.link} to='/skills'>
           <Button variant="contained" className={classes.linkButton}>
@@ -131,42 +132,6 @@ export default function VerticalTabs() {
 
       </Tabs>
       <TabPanel className={classes.tabContent} value={value} index={0}>
-        <div className='xp-item'>
-          <h3>University Of Oregon</h3>
-          <h5>B.S. Biological Sciences</h5>
-          <Divider className={classes.divider} />
-          <h5>Graduated March, 2020</h5>
-          <p>Coursework focused heavily on scientific data analysis, statistics and critical reasoning.
-            Relevant technologies ephasised inluded Matlab, Python (Numpy, Pandas, MatplotLib, etc.), and the full Microsoft Office Suite </p>
-          <br />
-          {/* <p>In addition to analytical skills, </p> */}
-        </div>
-      </TabPanel>
-      <TabPanel className={classes.tabContent} value={value} index={1}>
-        <div className='xp-item'>
-          <h3>American Preclinical Services</h3>
-          <h5>Pathology Technician</h5>
-          <Divider className={classes.divider} />
-          <h5>Employed 8/2020 - 6/2021</h5>
-          <p>Function as a member of a highly efficient team responsible for conducting a wide range of duties in the support of proprietary preclinical medical device trials for over 300 clients.
-            Core responsibilities include performance of gross pathological inspections/necropsies, collection and processing of data in accordance with GLP standards and practice high-quality FDA complaint documentation.
-          </p>
-          <br />
-          <p>Relevant software duties include the use of Python in support of data management/archival, as well as debugging lab equipment legacy code (Bash, Visual Basic) </p>
-        </div>
-      </TabPanel>
-      <TabPanel className={classes.tabContent} value={value} index={2}>
-        <div className='xp-item'>
-          <h3>Flatiron School</h3>
-          <h5>Software Engineering Student</h5>
-          <Divider className={classes.divider} />
-          <h5>Enrolled 6/2021 - Anticipated Graduation 11/2021</h5>
-          <p>Complete comprehensive software engineering coursework in the context of modern web infrastructure.
-            Coursework includes developing full-stack applications using in-demand frameworks such as React, Rails and Flask as well as 1500+ hours of hands-on experience building web-based applications and mastering OOP, RESTful and DRY practices.
-          </p>
-        </div>
-      </TabPanel>
-      <TabPanel className={classes.tabContent} value={value} index={3}>
         <div className='xp-item'>
           <h3>Freelance Developer</h3>
           <Divider className={classes.divider} />
@@ -205,6 +170,42 @@ export default function VerticalTabs() {
               </ul>
             </li>
           </ul>
+        </div>
+      </TabPanel>
+      <TabPanel className={classes.tabContent} value={value} index={1}>
+        <div className='xp-item'>
+          <h3>Flatiron School</h3>
+          <h5>Software Engineering Student</h5>
+          <Divider className={classes.divider} />
+          <h5>Enrolled 6/2021 - Anticipated Graduation 11/2021</h5>
+          <p>Complete comprehensive software engineering coursework in the context of modern web infrastructure.
+            Coursework includes developing full-stack applications using in-demand frameworks such as React, Rails and Flask as well as 1500+ hours of hands-on experience building web-based applications and mastering OOP, RESTful and DRY practices.
+          </p>
+        </div>
+      </TabPanel>
+      <TabPanel className={classes.tabContent} value={value} index={2}>
+        <div className='xp-item'>
+          <h3>American Preclinical Services</h3>
+          <h5>Pathology Technician</h5>
+          <Divider className={classes.divider} />
+          <h5>Employed 8/2020 - 6/2021</h5>
+          <p>Function as a member of a highly efficient team responsible for conducting a wide range of duties in the support of proprietary preclinical medical device trials for over 300 clients.
+            Core responsibilities include performance of gross pathological inspections/necropsies, collection and processing of data in accordance with GLP standards and practice high-quality FDA complaint documentation.
+          </p>
+          <br />
+          <p>Relevant software duties include the use of Python in support of data management/archival, as well as debugging lab equipment legacy code (Bash, Visual Basic) </p>
+        </div>
+      </TabPanel>
+      <TabPanel className={classes.tabContent} value={value} index={3}>
+        <div className='xp-item'>
+          <h3>University Of Oregon</h3>
+          <h5>B.S. Biological Sciences</h5>
+          <Divider className={classes.divider} />
+          <h5>Graduated March, 2020</h5>
+          <p>Coursework focused heavily on scientific data analysis, statistics and critical reasoning.
+            Relevant technologies ephasised inluded Matlab, Python (Numpy, Pandas, MatplotLib, etc.), and the full Microsoft Office Suite </p>
+          <br />
+          {/* <p>In addition to analytical skills, </p> */}
         </div>
       </TabPanel>
     </div>
